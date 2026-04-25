a378facba2f827326c94dbff8e72870faba4b257
Vikas <vikasthavaliya@gmail.com>
2026-04-25 15:58:29 +0530

    fix autofill + register form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -34,40 +34,4 @@
       {showPwd ? <EyeOff size={14} /> : <Eye size={14} />}
     </button>
   </div>
-</form><form onSubmit={submit} autoComplete="off" className="space-y-6">
-
-  {/* hidden chrome hack */}
-  <input type="text" name="fakeuser" style={{ display: "none" }} />
-  <input type="password" name="fakepass" style={{ display: "none" }} />
-
-  {/* Email */}
-  <input
-    name="email"
-    type="email"
-    value={form.email}
-    onChange={handle}
-    placeholder="Email"
-    autoComplete="off"
-    className="input"
-  />
-
-  {/* Password */}
-  <div className="relative">
-    <input
-      name="password"
-      type={showPwd ? "text" : "password"}
-      value={form.password}
-      onChange={handle}
-      placeholder="Password"
-      autoComplete="new-password"
-      className="input pr-10"
-    />
-    <button
-      type="button"
-      onClick={() => setShowPwd(!showPwd)}
-      className="absolute right-4 top-1/2 -translate-y-1/2"
-    >
-      {showPwd ? <EyeOff size={14} /> : <Eye size={14} />}
-    </button>
-  </div>
 </form>
