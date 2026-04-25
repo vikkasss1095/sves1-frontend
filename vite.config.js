e8b0d499c89fe70fbac2f339ea45e5e009b0187d
Vikas <vikasthavaliya@gmail.com>
2026-04-25 15:18:51 +0530

    fix vite + routing

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: "/",   // ✅ VERY IMPORTANT (blank screen fix)
+
   server: {
     port: 5173,
     proxy: {
