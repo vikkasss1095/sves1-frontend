6254e4a4815efbe609171544ebffccf17047fa85
Vikas <vikasthavaliya@gmail.com>
2026-05-04 23:23:49 +0530

    profile rout update

--- src/VendorDashboard/VendorLayout.jsx
+++ src/VendorDashboard/VendorLayout.jsx
@@ -159,14 +159,14 @@ export default function VendorLayout() {
           {/* 🔥 CREATE ACCOUNT BUTTON */}
           {!user?.profileCompleted ? (
             <button
-                onClick={() => window.location.href = "/vendor/profile"}
+              onClick={() => navigate("/vendor/profile")}
               className="bg-red-500 text-white px-4 py-1.5 rounded-lg text-sm"
             >
               Create Account
             </button>
           ) : (
             <button
-          onClick={() => window.location.href = "/vendor/profile"}
+              onClick={() => navigate("/vendor/profile")}
               className="bg-indigo-500 text-white px-4 py-1.5 rounded-lg text-sm"
             >
               Profile
